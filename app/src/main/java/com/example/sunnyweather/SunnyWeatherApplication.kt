fa8ec2d61f507027f548602ebe63712ca7ce3948
ZJS <zjs@ZJSdeMacBook-Air.local>
2025-11-28 11:29:16 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
@@ -0,0 +1,18 @@
+package com.example.sunnyweather
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+    companion object {
+
+        const val TOKEN = "Z8ua3Fx9SeY6FJjo"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
